b71fe5ea124d35480098fa8a715204bd068f9e04
Vuk Petrovic <vukpetrovic1@yahoo.com>
2015-12-02 15:07:23 -0500

    Third challenge completed

--- CTCI_3.js
+++ CTCI_3.js
@@ -106,6 +106,8 @@ console.log("Permutations? : " + isPerm(sampleString1, sampleString2));
 */
 
 function isPermBetter(str1, str2) {
+	if(str1.length != str2.length) return false;
+
 	if(str1.split('').sort().join() != str2.split('').sort().join()) {
 		return false;
 	}
